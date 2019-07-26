86ec3b5a0e88a8aea9c768ce26693147319fcd53
tobi47schi <tobii12345@gmail.com>
2019-07-26 22:25:53 +0200

    debug bump

--- security/secret.js
+++ security/secret.js
@@ -3,7 +3,7 @@ const secretKey = require('secret-key');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const passphrase = 'asdf'; //selbst zu setzen!!
+const passphrase = process.env.SECRET_PASSPHRASE; //selbst zu setzen!!
 
 var secret = secretKey.create(passphrase)
 
@@ -12,7 +12,7 @@ var secret = secretKey.create(passphrase)
 console.log("secret:" , secret);
 exports.securityCheck = (req, res, next) => {
   //console.log("Security Check called")
-
+console.log('im check')
   if(!req.headers.secret || !req.headers.iv || !req.headers.timestamp) {
     res.status(403).send("Secret, iv and timestamp needed");
   } else if (!secretKey.check(passphrase, req.headers.secret, req.headers.iv, req.headers.timestamp)) {
